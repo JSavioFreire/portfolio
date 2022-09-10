15d5f9ec41f2cce69032501e832eeb05659325ea
Sávio Freire <freiresavio7@gmail.com>
2022-09-10 11:43:16 -0300

    falta so animar scroll

---
+++ js/scroll.js
@@ -0,0 +1,10 @@
+let btPc = document.querySelectorAll('#menuprincipal ul li');
+
+btPc.forEach((el,index) => {
+    btPc[index].addEventListener('click',()=>{
+        console.log('clicquei o '+index)
+    })
+
+    //ainda em producao
+    
+});
